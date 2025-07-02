3697b7f798e52d4821e50b1956bb11f6e9a33351
Abhinav Choudhary <cabhinav2005@gmail.com>
2025-07-03 01:07:26 +0530

    upload

--- utils/uploadthing.ts
+++ utils/uploadthing.ts
@@ -1,3 +1,11 @@
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+  generateReactHelpers,
+} from "@uploadthing/react";
+
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
-import { generateReactHelpers } from "@uploadthing/react";
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
 export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
